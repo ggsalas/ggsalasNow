700626b5f2affff74296afe76678e7dd1797a071
Gabriel Salas <ggsalas@gmail.com>
2018-03-24 21:23:24 -0300

    #newUpdate seems eslint fix and prettier are working :D

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,20 @@ import React from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 
-import Container from '../components/container' 
+import Container from '../components/container'
 import { getPosts, getPost } from '../Api'
 import Modal from '../components/modal'
 const SITE = 'techcrunch.com'
 
 class Index extends React.Component {
-
   // Get data and render on the server
   static async getInitialProps() {
-    return await getPosts( SITE )
+    return await getPosts(SITE)
   }
 
-  constructor( props ) {
-    super( props )
-    this.state = { 
+  constructor(props) {
+    super(props)
+    this.state = {
       site: SITE,
       data: {
         found: this.props.data.found,
@@ -28,20 +27,19 @@ class Index extends React.Component {
     this.postsPerPage = 20
   }
 
-  onSiteChange = ( event ) => {
+  onSiteChange = event => {
     const site = event.target.value
 
-    if( event.target.value.length > 5 ) {
-      getPosts( site )
-      .then(( response ) => {
-        if( response ) {
-          this.setState({ 
+    if (event.target.value.length > 5) {
+      getPosts(site).then(response => {
+        if (response) {
+          this.setState({
             site,
             data: response.data
           })
         } else {
-          this.setState({ 
-            site, 
+          this.setState({
+            site,
             data: {},
             error: 'no se encuentra el blog'
           })
@@ -50,31 +48,31 @@ class Index extends React.Component {
     }
   }
 
-  onGoPost = ( e, post ) => {
+  onGoPost = (e, post) => {
     e.preventDefault()
-    Router.push(`/?postId=${post.ID}`, `/post?site=${this.state.site}&id=${post.ID}`, { shallow: true })
-      this.setState({
-        post
-      })
+    Router.push(
+      `/?postId=${post.ID}`,
+      `/post?site=${this.state.site}&id=${post.ID}`,
+      { shallow: true }
+    )
+    this.setState({
+      post
+    })
   }
 
-  dismissModal () {
+  dismissModal() {
     Router.push('/', '/', { shallow: true })
   }
 
   onLoadMore = () => {
     const nextPage = this.state.page + 1
 
-    getPosts( this.state.site, this.postsPerPage, nextPage )
-    .then(( response ) => {
-      if( response ) {
-        this.setState({ 
+    getPosts(this.state.site, this.postsPerPage, nextPage).then(response => {
+      if (response) {
+        this.setState({
           data: {
             found: this.state.data.found,
-            posts: [
-              ...this.state.data.posts,
-              ...response.data.posts
-            ],
+            posts: [...this.state.data.posts, ...response.data.posts]
           },
           page: nextPage
         })
@@ -85,46 +83,54 @@ class Index extends React.Component {
   render() {
     return (
       <Container display>
-        <div className='index'>
-          <section className='header'>
-            <label htmlFor='site' className='siteSearchLabel'>Ver publicaciones del Blog: </label>
-            <input 
-              className='siteSearch'
-              name = 'site'
-              defaultValue = { SITE }
-              onChange = { this.onSiteChange } 
+        <div className="index">
+          <section className="header">
+            <label htmlFor="site" className="siteSearchLabel">
+              Ver publicaciones del Blog:{' '}
+            </label>
+            <input
+              className="siteSearch"
+              name="site"
+              defaultValue={SITE}
+              onChange={this.onSiteChange}
             />
           </section>
-          {
-            this.props.url.query && this.props.url.query.postId &&
+          {this.props.url.query &&
+            this.props.url.query.postId && (
               <Modal
-                id = { this.props.url.query.postId }
-                post = { this.state.post }
-                onDismiss = { this.dismissModal }
+                id={this.props.url.query.postId}
+                post={this.state.post}
+                onDismiss={this.dismissModal}
               />
-          }
-          { 
-            this.state.data && this.state.data.posts
-            ? ( 
-                <ul className='postList'>
-                  { 
-                    this.state.data.posts.map(( post ) => (
-                      <li className='postItem'  key={post.ID} onClick={ e => this.onGoPost( e, post ) }>
-                        <span className='postTitle' dangerouslySetInnerHTML={{__html: post.title}}></span>
-                        <span className='postExcerpt' dangerouslySetInnerHTML={{__html: post.excerpt}}></span>
-                      </li>
-                    )) 
-                  }
-                </ul>
-              )
-            : <p>{ this.state.error }</p>
-          }
-          {
-            this.state.page < this.state.data.found / this.postsPerPage
-              ? <div onClick={ this.onLoadMore } className='loadMore'>Load More</div>
-              : null
-          }
-              
+            )}
+          {this.state.data && this.state.data.posts ? (
+            <ul className="postList">
+              {this.state.data.posts.map(post => (
+                <li
+                  className="postItem"
+                  key={post.ID}
+                  onClick={e => this.onGoPost(e, post)}
+                >
+                  <span
+                    className="postTitle"
+                    dangerouslySetInnerHTML={{ __html: post.title }}
+                  />
+                  <span
+                    className="postExcerpt"
+                    dangerouslySetInnerHTML={{ __html: post.excerpt }}
+                  />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>{this.state.error}</p>
+          )}
+          {this.state.page < this.state.data.found / this.postsPerPage ? (
+            <div onClick={this.onLoadMore} className="loadMore">
+              Load More
+            </div>
+          ) : null}
+
           <style jsx>{`
             .index {
               margin: 20px;
@@ -162,7 +168,7 @@ class Index extends React.Component {
               border-color: #000;
             }
             .postExcerpt {
-              font-size: .8em;
+              font-size: 0.8em;
             }
             .loadMore {
               cursor: pointer;
@@ -185,4 +191,3 @@ class Index extends React.Component {
 }
 
 export default Index
-
