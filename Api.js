3b158cb7190978800168601c0fac58a4dce635c4
Gabriel Salas <ggsalas@gmail.com>
2018-03-25 19:31:10 -0300

    #newUpdate - prettier js files :D

--- Api.js
+++ Api.js
@@ -2,42 +2,45 @@ import fetch from 'isomorphic-unfetch'
 
 const WP_PUBLIC = 'https://public-api.wordpress.com/rest/v1.1/sites/'
 
-export function getPosts( site, number = 20, page = 1 ) {
-  const get = fetch( `${WP_PUBLIC}${site}/posts/?number=${number}&page=${page}` )
-  .then( status )
-  .then( json )
-  .then( data => { return ({data}) } )
-  .catch( (reason) => {
-    console.log( 'Fetch error: ', reason )
-  })
+export function getPosts(site, number = 20, page = 1) {
+  const get = fetch(`${WP_PUBLIC}${site}/posts/?number=${number}&page=${page}`)
+    .then(status)
+    .then(json)
+    .then(data => {
+      return { data }
+    })
+    .catch(reason => {
+      console.log('Fetch error: ', reason)
+    })
 
   return get
 }
 
-export function getPost( site, postId ) {
-  const get = fetch( `${WP_PUBLIC}${site}/posts/${postId}` )
-  .then( status )
-  .then( json )
-  .then( data => { return ({post: data}) } )
-  .catch( (reason) => {
-    console.log( 'Fetch error: ', reason )
-  })
+export function getPost(site, postId) {
+  const get = fetch(`${WP_PUBLIC}${site}/posts/${postId}`)
+    .then(status)
+    .then(json)
+    .then(data => {
+      return { post: data }
+    })
+    .catch(reason => {
+      console.log('Fetch error: ', reason)
+    })
 
   return get
 }
 
-/* 
+/*
  * Private funcions
  */
-function status( response ) {  
-  if ( response.status >= 200 && response.status < 300 ) {  
-    return Promise.resolve( response )  
-  } else {  
-    return Promise.reject( new Error(response.statusText) )  
-  }  
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
 }
 
-function json( response ) {  
-  return response.json()  
+function json(response) {
+  return response.json()
 }
-
--- components/container.js
+++ components/container.js
@@ -1,37 +1,37 @@
 import React from 'react'
 
 export default function Container(props) {
-    return (
-      <div>
-        {
-          props.remove === true
-            ? <div>{ props.children }</div>
-            : (
-                <div className='container'>
-                  <section className='header'><h1 className='headerTitle'>Wordpress Blog Reader</h1></section>
-                  { props.children }
-                </div>
-              )
+  return (
+    <div>
+      {props.remove === true ? (
+        <div>{props.children}</div>
+      ) : (
+        <div className="container">
+          <section className="header">
+            <h1 className="headerTitle">Wordpress Blog Reader</h1>
+          </section>
+          {props.children}
+        </div>
+      )}
+      <style jsx>{`
+        @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+        :global(body) {
+          font-family: 'Roboto Mono', monospace;
+          color: #555;
+          margin: 0;
+          padding: 0;
         }
-        <style jsx>{`
-          @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
-          :global(body) {
-            font-family: 'Roboto Mono', monospace;
-            color: #555;
-            margin: 0;
-            padding: 0;
-          }
 
-          .container {
-            border-top: 8px solid #333;
-          }
-          .headerTitle {
-            text-align: center;
-            font-size: 30px;
-            margin: 20px 20px 30px 20px;
-            color: #333;
-          }
-        `}</style>
-      </div>
-    )
+        .container {
+          border-top: 8px solid #333;
+        }
+        .headerTitle {
+          text-align: center;
+          font-size: 30px;
+          margin: 20px 20px 30px 20px;
+          color: #333;
+        }
+      `}</style>
+    </div>
+  )
 }
